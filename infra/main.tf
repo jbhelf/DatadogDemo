e5d0cadb6922c536cea49ee1c15559fad05fd62d
jbhelf <justinbhelfman@gmail.com>
2025-08-30 08:37:30 -0400

    Terraform fix - output as single source of output truth

--- infra/main.tf
+++ infra/main.tf
@@ -196,7 +196,3 @@ resource "aws_instance" "web" {
     cost_center  = "ddemo"
   }
 }
-
-output "instance_id" { value = aws_instance.web.id }
-output "public_ip"   { value = aws_instance.web.public_ip }
-output "public_dns"  { value = aws_instance.web.public_dns }
